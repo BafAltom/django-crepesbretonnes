d5d5ac85921c211453990a1ab137ad3ece90d018
Thomas Chapeaux <chapeauxthomas@gmail.com>
2013-09-23 12:42:28 +0200

    root and blog app

---
+++ blog/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import patterns, url
+
+urlpatterns = patterns('blog.views',
+	url(r'^$', 'home'),
+	url(r'^article/(?P<id>\d+)/?$', 'view_article'),
+)
---
+++ blog/views.py
@@ -0,0 +1,14 @@
+# Create your views here.
+#-*- coding: utf-8 -*-
+from django.http import HttpResponse
+
+def home(request):
+	text = """<h1>Bienvenue sur mon blog !</h1>
+		  <p>Ton papa a fait l'amour avec ta maman au moins une fois</p>
+		"""
+	return HttpResponse(text)
+
+def view_article(request, id):
+	"""Affiche un article selon son identifiant (id)"""
+	text = "Vous avez demandé l'article n°{0} !".format(id)
+	return HttpResponse(text)
