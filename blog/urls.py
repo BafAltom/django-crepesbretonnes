394b6f4ea8d0af3d18c4a506c987b2192a3f3ec7
Thomas Chapeaux <chapeauxthomas@gmail.com>
2013-09-23 13:57:40 +0200

    Added tpl.html

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, url
 
-urlpatterns = patterns('blog.views',
+urlpatterns = patterns(
+    'blog.views',
     url(r'^$', 'home'),
     url(r'^article/(?P<id>\d+)/?$', 'view_article'),
+    url(r'^date$', 'tpl'),
 )
