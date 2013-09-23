72fc9d4814f7cde09322013e93e7217335612b30
Thomas Chapeaux <chapeauxthomas@gmail.com>
2013-09-23 15:57:29 +0200

    more blog template

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,20 @@
 # Create your views here.
 #-*- coding: utf-8 -*-
-from datetime import datetime
 
-from django.http import HttpResponse
+# from datetime import datetime
+
+# from django.http import HttpResponse
 from django.shortcuts import render
+
+from blog.models import Article
 
 
 def home(request):
-    text = """<h1>Bienvenue sur mon blog !</h1>
-          <p>C'est chouette.</p>
-        """
-    return HttpResponse(text)
+    articles = Article.objects.all()
+    return render(request, 'blog/article_list.html', {'articles': articles})
 
 
 def view_article(request, id):
     """Affiche un article selon son identifiant (id)"""
-    text = "Vous avez demandé l'article n°{0} !".format(id)
-    return HttpResponse(text)
-
-
-def tpl(request):
-    return render(request, 'blog/tpl.html', {'current_date': datetime.now()})
+    article = Article.objects.filter(id=id)
+    return render(request, 'blog/read.html', {'article': article})
