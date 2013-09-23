7be0694ca15a07844d52e1b5f45f671d7646b395
Thomas Chapeaux <chapeauxthomas@gmail.com>
2013-09-23 16:59:38 +0200

    added frigging commas

--- crepesbretonnes/settings.py
+++ crepesbretonnes/settings.py
@@ -126,11 +126,11 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware'
-    'django.contrib.auth.middleware.AuthenticationMiddleware'
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'blog'
+    'blog',
 )
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
