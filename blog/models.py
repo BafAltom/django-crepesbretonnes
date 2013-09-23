cf721f8e6ca20f1c231e0d29e49d3015be442d8e
Thomas Chapeaux <chapeauxthomas@gmail.com>
2013-09-23 15:28:10 +0200

    Added Article blog model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=12)
+    content = models.TextField(null=True)
+    date = models.DateTimeField(auto_now_add=True, auto_now=False, verbose_name="Publication Date")
+
+    def __unicode__(self):
+        return u"%s" % self.title
