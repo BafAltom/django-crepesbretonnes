fc0c392af156a9fcbf0ec82fa69873ddbcef785e
Thomas Chapeaux <chapeauxthomas@gmail.com>
2013-09-24 14:44:45 +0200

    super clever form

--- blog/forms.py
+++ blog/forms.py
@@ -7,3 +7,13 @@ class ContactForm(forms.Form):
     sender = forms.EmailField(label=u"Your email")
     sendCopy = forms.BooleanField(help_text=u"Do you want a copy of your mail sent to you?")
 
+    def clean(self):
+        cleaned_data = super(ContactForm, self).clean()
+        subject = cleaned_data.get('subject')
+        message = cleaned_data.get('message')
+        if subject and message:
+            if "pizza" in subject and "pizza" in message:
+                msg = u"Pizzas? In MY contact form?"
+                self._errors["message"] = self.error_class([msg])
+                del cleaned_data["message"]
+        return cleaned_data
