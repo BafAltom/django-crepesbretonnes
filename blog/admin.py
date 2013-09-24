658c83c2a89961d5b96e79276a89221191ecc149
Thomas Chapeaux <chapeauxthomas@gmail.com>
2013-09-24 13:53:32 +0200

    Added ArticleAdmin class

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from blog.models import Category, Article
 
+
+class ArticleAdmin(admin.modelAdmin):
+    list_display = ('title', 'author', 'date')
+    list_filter = ('author', 'category')
+    date_hierarchy = 'date'
+    ordering = ('date', )
+    search_fields = ('title', 'content')
+
+
 admin.site.register(Category)
-admin.site.register(Article)
+admin.site.register(Article, ArticleAdmin)
