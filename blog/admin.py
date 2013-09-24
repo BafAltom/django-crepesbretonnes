e697f58bdc10b55b617169f90d8b3c00245896a1
Thomas Chapeaux <chapeauxthomas@gmail.com>
2013-09-24 13:58:25 +0200

    Added content_peek for list display

--- blog/admin.py
+++ blog/admin.py
@@ -3,11 +3,21 @@ from blog.models import Category, Article
 
 
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'date')
+    list_display = ('title', 'author', 'date', 'content_peek')
     list_filter = ('author', 'category')
     date_hierarchy = 'date'
     ordering = ('date', )
     search_fields = ('title', 'content')
+
+    def content_peek(self, article):
+        """
+        Return the content shortened to 40 characters
+        """
+        text = article.content[0:40]
+        if len(article.content) > 40:
+            return '%s...' % text
+        else:
+            return text
 
 
 admin.site.register(Category)
