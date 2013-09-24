3577d7210adeb1a9b27a4950b5474e44b5c6fd25
Thomas Chapeaux <chapeauxthomas@gmail.com>
2013-09-24 15:59:55 +0200

    typo

--- blog/forms.py
+++ blog/forms.py
@@ -6,7 +6,7 @@ class ContactForm(forms.Form):
     subject = forms.CharField(max_length=100)
     message = forms.CharField(widget=forms.Textarea)
     sender = forms.EmailField(label=u"Your email")
-    sendCopy = forms.BooleanField(help_text=u"Do you want a copy of your mail sent to you?", require=False)
+    sendCopy = forms.BooleanField(help_text=u"Do you want a copy of your mail sent to you?", required=False)
 
     def clean(self):
         cleaned_data = super(ContactForm, self).clean()
