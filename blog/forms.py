b49d7e45892de118a0ec764e8e26b650d96a620f
Thomas Chapeaux <chapeauxthomas@gmail.com>
2013-09-24 14:46:55 +0200

    A required boolean field makes no sense :)

--- blog/forms.py
+++ blog/forms.py
@@ -5,7 +5,7 @@ class ContactForm(forms.Form):
     subject = forms.CharField(max_length=100)
     message = forms.CharField(widget=forms.Textarea)
     sender = forms.EmailField(label=u"Your email")
-    sendCopy = forms.BooleanField(help_text=u"Do you want a copy of your mail sent to you?")
+    sendCopy = forms.BooleanField(help_text=u"Do you want a copy of your mail sent to you?", require=False)
 
     def clean(self):
         cleaned_data = super(ContactForm, self).clean()
